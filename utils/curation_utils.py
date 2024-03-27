d0cb70f3e082fd20bcae6dd09baeb19b1999fd77
Chia-Chun Wu <ccwu0918@gmail.com>
2024-03-27 23:30:23 +0800

    Update curation_utils.py

--- utils/curation_utils.py
+++ utils/curation_utils.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer
-import openai
+from openai import OpenAI
 import numpy as np
+client = OpenAI()
 
 def split_text(text, max_length):
     """將長文本切割為多個最大長度的子串"""
@@ -24,11 +25,23 @@ def to_openai_embeddings(text_list: list, max_length=2048):
 
         part_embeddings = []
         for part in text_parts:
-            response = openai.Embedding.create(
+            # the old version, e.g. `pip install openai==0.28`
+            # no longer supported in openai>=1.0.0
+
+            # response = openai.Embedding.create(
+            #     input=[part],
+            #     model="text-embedding-3-small"
+            # )
+            # response = openai.Engine("text-embedding-3-small").embeddings(
+            #     input=[part]
+            # )
+            # embeddings = response['data'][0]['embedding']
+                        
+            response = client.embeddings.create(
                 input=[part],
                 model="text-embedding-3-small"
-            )
-            embeddings = response['data'][0]['embedding']
+            )            
+            embeddings = response.data[0].embedding
             part_embeddings.append(embeddings)
         
         # 將同一文本的多部分嵌入向量平均處理以獲取整體嵌入向量
@@ -42,14 +55,13 @@ def to_openai_embeddings(text_list: list, max_length=2048):
     return embeddings_list
 
 # def to_openai_embeddings(text_list: list):
-#     # 使用 OpenAI 的 "text-embedding-3-small" 模型來獲取文本嵌入向量
-#     embeddings_response = openai.Embedding.create(
-#         input=text_list,
-#         model="text-embedding-3-small"
-#     )
-    
-#     # 從回應中提取嵌入向量並轉換為 list of lists 形式
-#     embeddings_list = [embedding['embedding'] for embedding in embeddings_response['data']]
+#     embeddings_list = []
+#     for text in text_list:
+#         response = openai.Engine("text-embedding-3-small").embeddings(
+#             input=[text]
+#         )
+#         embeddings = response['data'][0]['embedding']
+#         embeddings_list.append(embeddings)
     
 #     # 將嵌入向量轉換為 NumPy 陣列來獲取其形狀
 #     embeddings_array = np.array(embeddings_list)
@@ -59,13 +71,14 @@ def to_openai_embeddings(text_list: list, max_length=2048):
 #     return embeddings_list
 
 # def to_openai_embeddings(text_list: list):
-#     embeddings_list = []
-#     for text in text_list:
-#         response = openai.Engine("text-embedding-3-small").embeddings(
-#             input=[text]
-#         )
-#         embeddings = response['data'][0]['embedding']
-#         embeddings_list.append(embeddings)
+#     # 使用 OpenAI 的 "text-embedding-3-small" 模型來獲取文本嵌入向量
+#     embeddings_response = openai.Embedding.create(
+#         input=text_list,
+#         model="text-embedding-3-small"
+#     )
+    
+#     # 從回應中提取嵌入向量並轉換為 list of lists 形式
+#     embeddings_list = [embedding['embedding'] for embedding in embeddings_response['data']]
     
 #     # 將嵌入向量轉換為 NumPy 陣列來獲取其形狀
 #     embeddings_array = np.array(embeddings_list)
